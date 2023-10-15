43c805f8d9f209b67ed26c6f6f44799485d6ffac
Amir Famili <44036070+amirmohammadfamili@users.noreply.github.com>
2023-10-14 23:11:11 -0500

    Add new register and log in page with formik

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,93 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-class Login extends Component {
 
-    render() {
-        return (
-            <>
+const Login = () => {
 
-                <div className="log-in">
-                    <form className="form-log-in" action="index.html" enctype="multipart/form-data">
+    return (
+        <>
+
+
+            <div className="log-in">
+
+
+
+                <Formik initialValues={{
+                    email: "",
+                    password: "",
+
+                }}
+
+                    onSubmit={(values, { setSubmitting }) => {
+                        values.email = '';
+                        values.password = '';
+
+                        setTimeout(() => {
+                            setSubmitting(false);
+                        }, 2000)
+                    }}
+
+
+                    validate={(values) => {
+                        const errors = {};
+
+                        if (values.email === '') {
+
+                            errors.email = 'Provide valid email address';
+                        } else if (!values.email.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+                            errors.email = 'This is not a valid email address'
+                        }
+
+                        if (values.password == '') {
+
+                            errors.password = 'Password can not be empty';
+                        } else if (values.password.length < 8) {
+                            errors.password = 'You must use at least 8 characters';
+                        }
+
+
+                        return errors;
+                    }}
+
+
+
+                >
+                    {({isSubmitting }) => (
+                    <Form className="form-log-in" action="index.html" enctype="multipart/form-data">
 
                         <h1 className="title">Log In</h1>
                         <hr />
                         <div className="input-control">
-
                             <div className="input-control">
                                 <label for="email">Email</label>
-                                <input type="email" name="email" className="email-log-in" />
-                                <div className="error"></div>
+                                <Field type="email" name="email" className="email-log-in" />
+                                <ErrorMessage name='email' >
+                                    {(errorMsg) => <div className="error">{errorMsg}</div>}
+                                </ErrorMessage>
                             </div>
 
                             <div className="input-control">
                                 <label for="password">Password</label>
-                                <input type="password" name="password" className="password-log-in" />
-                                <div className="error"></div>
+                                <Field type="password" name="password" className="password-log-in" />
+                                <ErrorMessage name='password' >
+                                    {(errorMsg) => <div className="error">{errorMsg}</div>}
+                                </ErrorMessage>
                             </div>
 
-                            <button className="submit" type="submit">Log In</button>
+
+                           
+                            <button className="submit" type="submit" disabled={isSubmitting}>
+                                {isSubmitting ? <span><i class="fas fa-circle-notch fa-spin"></i></span> : <span>Log in</span>}</button>
+
                         </div>
 
-                    </form>
-                </div>
-                </>
-        );
-    }
+                    </Form>)}
+                   
+                </Formik>
+            </div>
+        </>
+    );
 }
+
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,53 +1,151 @@
-import React, { Component } from 'react';
-
-class Regisgter extends Component {
-
-    render() {
-        return (
-            <>
-                <div className="registration">
-                    <form className="form-registration" action="index.html" enctype="multipart/form-data">
-                        <h1 className="title">Registration</h1>
-                        <hr />
-                        <div className="input-control ">
-                            <label for="fname">First name</label>
-                            <input type="text" name="fname" className="fname" />
-                            <div className="error"></div>
-                        </div>
-                        <div className="input-control">
-                            <label for="lname">Last name</label>
-                            <input type="text" name="lname" className="lname" />
-                            <div className="error"></div>
-                        </div>
-                        <div className="input-control">
-                            <label for="age">Age</label>
-                            <input type="number" name="age" className="age" min="15" max="90" value="20" />
-                            <div className="error"></div>
-                        </div>
-
-                        <div className="input-control">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" className="email" />
-                            <div className="error"></div>
-                        </div>
-                        <div className="input-control">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" className="password" />
-                            <div className="error"></div>
-                        </div>
-                        <div className="input-control">
-                            <label for="passworda">Password again</label>
-                            <input type="password" name="passworda" className="passworda" />
-                            <div className="error"></div>
-                        </div>
-                        <button className="submit" type="submit">Sign Up</button>
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
-                    </form>
-                </div>
-            </>
-        );
-    }
+const Regisgter = () => {
+
+
+    return (
+        <>
+            <div className="registration">
+                <Formik initialValues={{
+                    fname: "",
+                    lname: "",
+                    age: "",
+                    email: "",
+                    password: "",
+                    confirmPassword: ""
+                }}
+
+                    onSubmit={(values,{setSubmitting})=>{
+                        values.fname ='';
+                        values.lname ='';
+                        values.age ='';
+                        values.email ='';
+                        values.password ='';
+                        values.confirmPassword ='';
+                       setTimeout(()=>{
+                        setSubmitting(false);
+                       },2000)
+                    }}
+          
+
+                    validate={(values) => {
+                        const errors = {};
+
+
+                        if (values.fname === '') {
+
+                            errors.fname = 'Firstname is required';
+                        }
+
+
+                        if (values.lname === '') {
+
+                            errors.lname = 'Lastname is required';
+                        }
+
+
+                        if (values.age === '') {
+
+                            errors.age = 'age is required';
+                        } else if (values.age < 20) {
+                            errors.age = 'You must be older than 20 years';
+                        }
+
+
+
+                        if (values.email === '') {
+
+                            errors.email = 'Provide valid email address';
+                        } else if (!values.email.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+                            errors.email = 'This is not a valid email address'
+                        }
+
+                        if (values.password == '') {
+
+                            errors.password = 'Password can not be empty';
+                        } else if (values.password.length < 8) {
+                            errors.password = 'You must use at least 8 characters';
+                        }
+
+                        if (values.confirmPassword == '') {
+                            errors.confirmPassword = "Password can not be empty";
+                        } else if (values.confirmPassword !== values.password) {
+                            errors.confirmPassword = "Password does not match";
+                        }
+                        return errors;
+                    }}
+
+
+
+                >
+                   
+
+                    {({isSubmitting})=>(
+                         <Form className="form-registration" action="index.html" enctype="multipart/form-data">
+                         
+                         <h1 className="title">Registration</h1>
+                         <hr />
+                         <div className="input-control ">
+                             <label for="fname">First name</label>
+                             <Field type="text" name="fname" className="fname" />
+                             <ErrorMessage name='fname' >
+                                 {(errorMsg) => <div className="error">{errorMsg}</div>}
+                             </ErrorMessage>
+
+
+                         </div>
+                         <div className="input-control ">
+                             <label for="lname">Last name</label>
+                             <Field type="text" name="lname" className="lname" />
+                             <ErrorMessage name='lname' >
+                                 {(errorMsg) => <div className="error">{errorMsg}</div>}
+                             </ErrorMessage>
+                         </div>
+                         <div className="input-control">
+                             <label for="age">Age</label>
+                             <Field type="number" name="age" className="age" min="15" max="90" />
+                             <ErrorMessage name='age' >
+                                 {(errorMsg) => <div className="error">{errorMsg}</div>}
+                             </ErrorMessage>
+                         </div>
+
+                         <div className="input-control">
+                             <label for="email">Email</label>
+                             <Field type="email" name="email" className="email" />
+                             <ErrorMessage name='email' >
+                                 {(errorMsg) => <div className="error">{errorMsg}</div>}
+                             </ErrorMessage>
+                         </div>
+                         <div className="input-control">
+                             <label for="password">Password</label>
+                             <Field type="password" name="password" />
+                             <ErrorMessage name='password' >
+                                 {(errorMsg) => <div className="error">{errorMsg}</div>}
+                             </ErrorMessage>
+                         </div>
+                         <div className="input-control">
+                             <label for="confirmPassword">Password again</label>
+                             <Field type="password" name="confirmPassword" className="passworda" />
+                             <ErrorMessage name="confirmPassword" >
+                                 {(errorMsg) => <div className="error">{errorMsg}</div>}
+                             </ErrorMessage>
+                         </div>
+                         <button className="submit" type="submit" disabled={isSubmitting}>
+                            {isSubmitting ? <span> <i class="fas fa-circle-notch fa-spin"></i></span>:<span>Sign in</span> }</button>
+
+
+                     </Form>
+                    )}
+                       
+               
+
+                </Formik>
+            </div>
+        </>
+    );
 }
+
 
 export default Regisgter;
